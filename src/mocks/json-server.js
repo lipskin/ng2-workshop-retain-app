428714585182cbb4bfc851ee523b743833ad275e
Yishan Wen <wittywys@gmail.com>
2016-12-04 20:05:30 +0800

    add auth in mock server

--- src/mocks/json-server.js
+++ src/mocks/json-server.js
@@ -6,8 +6,34 @@ var middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
+
+var token = 'jwt_token';
+
+function isAuthorized(req) {
+  authorization_token = 'Bearer ' + token;
+  if (req.headers.authorization === authorization_token) {
+    return true;
+  }
+  return false;
+}
+
+server.get('/auth', function(req, res) {
+  return res.json({
+    token: token
+  })
+})
+
+server.use(function (req, res, next) {
+ if (isAuthorized(req)) { // add your authorization logic here
+   next() // continue to JSON Server router
+ } else {
+   res.sendStatus(401)
+ }
+})
+
 server.use(router);
 
 server.listen(3000, function () {
   console.log('JSON Server is running')
 });
+
