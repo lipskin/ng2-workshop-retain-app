a5e1e9c73a9b5cb085e37dc3df7c287b194d44a8
Yishan Wen <wittywys@gmail.com>
2016-12-04 22:42:08 +0800

    add router

--- src/app/features/app-bar/app-bar.component.ts
+++ src/app/features/app-bar/app-bar.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../core/auth.service';
+
 @Component({
   selector: 'rt-app-bar',
   templateUrl: './app-bar.component.html',
   styleUrls: ['./app-bar.component.css']
 })
 export class AppBarComponent implements OnInit {
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() { }
+
+  signOut() {
+    this.authService.signOut();
+  }
 }
